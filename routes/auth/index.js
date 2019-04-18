22153297ed00e36584e9356701196d987badd3de
Brow1230 <43067741+Brow1230@users.noreply.github.com>
2019-04-18 12:55:51 -0400

    comments on auth,

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -4,6 +4,9 @@ const sanitizeBody = require('../../middleware/sanitizeBody')
 const authorize = require('../../middleware/auth')
 const debug = require('debug')('app:authRouter')
 
+///
+/// SIGN UP PAGE
+///
 router.post('/users', sanitizeBody, async(req,res) => {
     try{
         let newUser = new User(req.sanitzedBody)
@@ -39,60 +42,42 @@ router.post('/users', sanitizeBody, async(req,res) => {
     }
 })
 
+//
+//  FUNCTION TO UPDATE PASSWORDS, CAN BE EXPANDED TO INCLUDE OTHER INFORMATION.
+//
 const update = (overwrite = false) => async (req,res) => { 
     try{
-        // debug(req.sanitizedBody)
-
         const user = await User.findOneAndUpdate(
             req.sanitzedBody.email,
             req.sanitzedBody.password,
-            {
-                new:true,
-                overwrite,
-                runValidators:true
-            }
+            { new:true, overwrite, runValidators:true }
         )
-        res.status(200).send({
-            data:user       
+        res.status(201).send({
+            data:{
+                user:user,
+                message:"password changed Successfully"
+            }       
         })
     }
     catch(err) {
-        debug(err)
-        res.status(404).send({
-            data: "error" + err
-        })    }
+        res.status(500).send({
+            errors: [{
+                status: 'Internal Server Error',
+                code: '500',
+                title: 'Problem Saving document to the database.'
+            }]
+        })
+    }
 }
 
+//
+//  PASSWORD CHANGING ROUTE
+//
 router.patch('/users', sanitizeBody, authorize, update((overwrite = false))); 
 
-// router.patch('/users', sanitizeBody, authorize, async(req,res) => {
-//     try{
-//         debug(req.sanitzedBody.email)
-
-
-//         // const user = await User.findOneAndUpdate({
-//         //     email:sanitzedBody.email,
-//         //     password: sanitizedBody.password
-//         // })
-//         // debug('user :' + user)
-//         // console.log('hello')
-
-//         // let newPassword = req.sanitzedBody
-//         // await newPassword.save()
-//         res.status(201).send({
-//             data: 'Password Changed'
-//         })
-//     }catch (err){
-//         res.status(500).send({
-//             errors: [{
-//                 status: 'Internal Server Error',
-//                 code: '500',
-//                 title: 'Problem Saving document to the database.'
-//             }]
-//         })
-//     }
-// })
-
+//
+//  LOGIN ROUTE
+//
 router.post('/users/token', sanitizeBody, async(req,res) => {
     const {email, password} = req.sanitzedBody
     const user = await User.authenticate(email,password)
@@ -110,6 +95,9 @@ router.post('/users/token', sanitizeBody, async(req,res) => {
     res.send({data: user.generateAuthToken()})
 })
 
+//
+//  USER PROFILE ROUTE
+//
 router.get('/users/me', authorize ,async(req,res) =>{
     const user = await User.findById(req.user._id)
     res.send({data:user})
