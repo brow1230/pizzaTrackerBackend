b15a0ea6c5237a315c22614425b776c15afa9d68
Brow1230 <43067741+Brow1230@users.noreply.github.com>
2019-04-26 13:34:07 -0400

    added production env

--- startup/database.js
+++ startup/database.js
@@ -3,8 +3,12 @@ const config = require('config')
 module.exports = () => {
     const mongoose = require('mongoose')
     const dbconfig = config.get('db')
+    let credentials = ''
+    if(process.env.NODE_ENV === 'production'){
+        credentials = `${dbconfig.user}:${dbconfig.password}`
+    }
     mongoose.connect(
-        `mongodb://${dbconfig.host}:${dbconfig.port}/${dbconfig.dbName}`,
+        `mongodb://${credentials}${dbconfig.host}:${dbconfig.port}/${dbconfig.dbName}`,
         {useNewUrlParser:true}
     )
     .then(()=>{
